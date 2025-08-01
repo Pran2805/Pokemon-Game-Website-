937b38908e275d8046f3a74405e503a2ee3999ad
Pran2805 <pranavshinde.as@gmail.com>
2025-08-01 18:12:28 +0530

    Player movement

---
+++ js/constants.js
@@ -0,0 +1,4 @@
+const mapPosWidth = -425
+const mapPosHeight = -390
+
+const speed = 3
--- js/index.js
+++ js/index.js
@@ -4,36 +4,86 @@ const context = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-context.fillStyle = '#fff'
-context.fillRect(0, 0, canvas.width, canvas.height)
-
-
-// rendering map
 const image = new Image()
-image.src = '../tile_assets/Pellet Town.png'
-// image takes some time to load that's why we used the onload method
-
-
-
-// player image load
 const playerImage = new Image()
+
+image.src = '../tile_assets/Pellet Town.png'
 playerImage.src = '../assets/Images/playerDown.png'
 
-image.onload = () => {
-    context.drawImage(image, -590, -400)
+// Track image loading
+let imagesLoaded = 0
+function checkAllImagesLoaded() {
+    imagesLoaded++
+    if (imagesLoaded === 2) {
+        animate()
+    }
+}
+
+image.onload = checkAllImagesLoaded
+playerImage.onload = checkAllImagesLoaded
+
+const background = new Sprite({
+    position: {
+        x: mapPosWidth,
+        y: mapPosHeight
+    },
+    image: image
+})
+
+const keys = {
+    w: { pressed: false },
+    a: { pressed: false },
+    s: { pressed: false },
+    d: { pressed: false }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    context.fillStyle = '#fff'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    background.draw(context)
+
     context.drawImage(
         playerImage,
         0,
         0,
-        playerImage.width/4,
+        playerImage.width / 4,
         playerImage.height,
-        canvas.width / 2 - playerImage.width,
-        canvas.height / 2 - playerImage.height,
-        playerImage.width/4,
+        canvas.width / 2 - (playerImage.width / 4) / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
         playerImage.height
     )
+    if(keys.w.pressed){
+        background.position.y += speed
+    }
+    if(keys.s.pressed){
+        background.position.y -= speed
+    }
+    if(keys.a.pressed){
+        background.position.x += speed
+    }
+    if(keys.d.pressed){
+        background.position.x -= speed
+    }
+
+    
 }
 
-
-
-
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w': keys.w.pressed = true; break
+        case 'a': keys.a.pressed = true; break
+        case 's': keys.s.pressed = true; break
+        case 'd': keys.d.pressed = true; break
+    }
+})
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w': keys.w.pressed = false;; break
+        case 'a': keys.a.pressed = false; break
+        case 's': keys.s.pressed = false; break
+        case 'd': keys.d.pressed = false; break
+    }
+})
